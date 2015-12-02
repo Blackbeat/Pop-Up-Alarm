b280e514b4c8437cbc4c47bf74c3d898be418321
segoist43 <segoist43@gmail.com>
2015-12-02 13:09:10 +0900

    initial commit

---
+++ Pop-Up Alarm/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSimple
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.nudMain = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuView = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuSimple = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClock = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExpert = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuHelp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.nudMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'nudMain
+        '
+        Me.nudMain.Location = New System.Drawing.Point(42, 38)
+        Me.nudMain.Maximum = New Decimal(New Integer() {59, 0, 0, 0})
+        Me.nudMain.Name = "nudMain"
+        Me.nudMain.Size = New System.Drawing.Size(55, 20)
+        Me.nudMain.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(103, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "분 뒤 알람을 울려드립니다."
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile, Me.mnuView, Me.mnuHelp})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(60, 20)
+        Me.mnuFile.Text = "파일 (&F)"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.X), System.Windows.Forms.Keys)
+        Me.mnuExit.Size = New System.Drawing.Size(157, 22)
+        Me.mnuExit.Text = "종료 (&X)"
+        '
+        'mnuView
+        '
+        Me.mnuView.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuSimple, Me.mnuClock, Me.mnuExpert})
+        Me.mnuView.Name = "mnuView"
+        Me.mnuView.Size = New System.Drawing.Size(61, 20)
+        Me.mnuView.Text = "보기 (&V)"
+        '
+        'mnuSimple
+        '
+        Me.mnuSimple.Checked = True
+        Me.mnuSimple.CheckOnClick = True
+        Me.mnuSimple.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.mnuSimple.Enabled = False
+        Me.mnuSimple.Name = "mnuSimple"
+        Me.mnuSimple.Size = New System.Drawing.Size(152, 22)
+        Me.mnuSimple.Text = "심플 모드"
+        '
+        'mnuClock
+        '
+        Me.mnuClock.CheckOnClick = True
+        Me.mnuClock.Name = "mnuClock"
+        Me.mnuClock.Size = New System.Drawing.Size(152, 22)
+        Me.mnuClock.Text = "시계 모드"
+        '
+        'mnuExpert
+        '
+        Me.mnuExpert.CheckOnClick = True
+        Me.mnuExpert.Name = "mnuExpert"
+        Me.mnuExpert.Size = New System.Drawing.Size(152, 22)
+        Me.mnuExpert.Text = "전문가 모드"
+        '
+        'mnuHelp
+        '
+        Me.mnuHelp.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem2, Me.ToolStripMenuItem3})
+        Me.mnuHelp.Name = "mnuHelp"
+        Me.mnuHelp.Size = New System.Drawing.Size(75, 20)
+        Me.mnuHelp.Text = "도움말 (&H)"
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(89, 22)
+        Me.ToolStripMenuItem2.Text = "??"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(89, 22)
+        Me.ToolStripMenuItem3.Text = "???"
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(187, 78)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(56, 21)
+        Me.btnStart.TabIndex = 3
+        Me.btnStart.Text = "빼액!"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Location = New System.Drawing.Point(125, 78)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(56, 21)
+        Me.btnStop.TabIndex = 4
+        Me.btnStop.Text = "싫어!"
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 1000
+        '
+        'frmSimple
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 111)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.nudMain)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmSimple"
+        Me.Text = "Pop-Up Alarm"
+        CType(Me.nudMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents nudMain As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents mnuFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuView As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuHelp As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents btnStop As System.Windows.Forms.Button
+    Friend WithEvents mnuExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuSimple As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuClock As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuExpert As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Timer As System.Windows.Forms.Timer
+
+End Class
---
+++ Pop-Up Alarm/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class frmSimple
+    Private settime As Integer
+    Private origtime As Integer
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        settime = CInt(nudMain.Value)
+        If settime = 0 Then
+            MsgBox("시간을 설정해주십시오.", vbOKOnly + vbExclamation, "오류")
+        Else
+            Timer.Enabled = True
+        End If
+
+        If Timer.Enabled = True Then
+            btnStart.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        If Timer.Enabled = False Then
+            Me.Close()
+        Else
+            btnStart.Enabled = True
+            Timer.Enabled = False
+            nudMain.Value = 0
+            nudMain.Focus()
+        End If
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        Dim MsgAnswer As Integer
+
+        Timer.Enabled = True
+        origtime += 1
+        If settime = origtime Then
+            MsgAnswer = MsgBox("빼애애애애액!", MsgBoxStyle.Information + vbOKOnly)
+            If MsgAnswer = MsgBoxResult.Ok Then
+                btnStart.Enabled = True
+                origtime = 0
+                nudMain.Value = 0
+                Timer.Enabled = False
+            End If
+        End If
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub mnuSimple_Click(sender As Object, e As EventArgs) Handles mnuSimple.Click
+        If mnuSimple.Checked = True Then
+            mnuSimple.Enabled = False
+            mnuClock.Checked = False
+            mnuClock.Enabled = True
+            mnuExpert.Checked = False
+            mnuExpert.Enabled = True
+        End If
+    End Sub
+
+    Private Sub mnuClock_Click(sender As Object, e As EventArgs) Handles mnuClock.Click
+        Me.Hide()
+        frmClock.Show()
+        If mnuClock.Checked = True Then
+            mnuSimple.Enabled = True
+            mnuSimple.Checked = False
+            mnuClock.Enabled = False
+            mnuExpert.Checked = False
+            mnuExpert.Enabled = True
+        End If
+    End Sub
+
+    Private Sub mnuExpert_Click(sender As Object, e As EventArgs) Handles mnuExpert.Click
+        Me.Hide()
+        frmExpert.Show()
+        If mnuExpert.Checked = True Then
+            mnuSimple.Enabled = True
+            mnuSimple.Checked = False
+            mnuClock.Enabled = True
+            mnuClock.Checked = False
+            mnuExpert.Enabled = False
+        End If
+    End Sub
+End Class
